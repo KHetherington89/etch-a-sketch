bb8d122965235bf5d5cb74283949d17f3d98a923
Kieren Hetherington <98293613+KHetherington89@users.noreply.github.com>
2023-02-16 20:01:57 +0000

    Add event listeners and a colour change function.
    The grid generation has been tweaked, and the grid elements now change to
    a hard coded colour on mouse entry.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,4 @@
 const gridContainer = document.getElementById("gridContainer");
-const gridElement = document.createElement("div");
-    gridElement.classList.add("gridElement");
-/*     gridElement.textContent="A"; */
 
 let gridSize = prompt("What grid size you want?");
 console.log(gridSize);
@@ -9,13 +6,20 @@ console.log(gridSize);
 function buildGrid(gridSize){
     gridContainer.style.cssText += `grid-template-columns: repeat(${gridSize}, 1fr); 
                                     grid-template-rows: repeat(${gridSize}, 1fr);
-                                    row-gap: 1px;
-                                    column-gap: 1px;`
+                                    column-gap: 1px;
+                                    row-gap: 1px;`                                   
     let i = 0;
     while(i<(gridSize*gridSize)){
-        gridContainer.appendChild(gridElement.cloneNode(true));
+        const gridElement = document.createElement("div");
+        gridContainer.appendChild(gridElement);
+        gridElement.classList.add("gridElement");
+        gridElement.addEventListener("mousedown" , (e)=> colorChange(e))
         i++;
     }
+}
+
+function colorChange(e){
+    e.target.style.backgroundColor = "yellow";
 }
 
 buildGrid(gridSize);
